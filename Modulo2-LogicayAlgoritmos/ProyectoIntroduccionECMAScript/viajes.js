0ca2640bbe73347950a37b63271f8eb059b25bd5
Emi-Ing04 <emigzmz@gmail.com>
2026-05-27 23:00:45 -0600

    Añadir estilos para tarjetas de viaje y mostrar itinerario en el DOM

--- Modulo2-LogicayAlgoritmos/ProyectoIntroduccionECMAScript/viajes.js
+++ Modulo2-LogicayAlgoritmos/ProyectoIntroduccionECMAScript/viajes.js
@@ -29,19 +29,29 @@ export const registrarDestino = (destino, fecha, transporte) => {
         costo: calcularCosto(destino, transporte) // Se calcula el costo usando la función interna
     };
     destinos.push(nuevoViaje); // Se agrega el nuevo viaje al array de destinos
-    console.log(`Destino registrado: ${destino} el ${fecha} usando ${transporte}. Costo estimado: $${nuevoViaje.costo}`);
     };
 
     // Función flecha exportada para mostrar itinerario usando Foreach
 export const mostrarItinerario = () => {
+    // Se obtiene el contenedor del DOM para mostrar los viajes
+    const contendor = document.getElementById("lista-viajes");
+    
+    contenedor.innerHTML = ""; // Limpiamos el contenedor antes de mostrar el itinerario
+
+
     console.log("✈️  --- ITINERARIO DE VIAJES PLANIFICADOS --- ✈️\n");
 
     // Automatización del recorrido con forEach para mostrar cada destino registrado
     destinos.forEach(viaje => {
-        console.log('Destino: ${viaje.destino}');
-        console.log('Fecha: ${viaje.fecha}');
-        console.log('Transporte: ${viaje.transporte}');
-        console.log('Costo: $${viaje.costo}');
-        console.log('-----------------------------');
+       
+        contenedor.innerHTML += `
+            <div class="tarjeta-viaje">
+                <h3>✈️ Destino: ${viaje.destino}</h3>
+                <p><strong>📅 Fecha:</strong> ${viaje.fecha}</p>
+                <p><strong>🚂 Transporte:</strong> ${viaje.transporte}</p>
+                <p><strong>💰 Costo Total:</strong> $${viaje.costo} USD</p>
+            </div>
+        `; // Se agrega cada viaje al contenedor del DOM
+
     });
 };
