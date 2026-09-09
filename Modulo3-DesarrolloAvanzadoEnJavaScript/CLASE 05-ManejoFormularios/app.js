eecd779cc88b836a8dc2016c616a705f485bca9f
Emi-Ing04 <emigzmz@gmail.com>
2026-09-08 19:35:38 -0600

    feat: enhance form validation with additional checks for required fields and input formats

--- Modulo3-DesarrolloAvanzadoEnJavaScript/CLASE 05-ManejoFormularios/app.js
+++ Modulo3-DesarrolloAvanzadoEnJavaScript/CLASE 05-ManejoFormularios/app.js
@@ -1,4 +1,4 @@
-document.getElementById('registroEvento').addEventListener('submit', function(event) {
+document.getElementById('registroEvento').addEventListener('submit', (event) => {
       event.preventDefault(); // Evita el envío automático del formulario
 
       // Variables
@@ -11,11 +11,39 @@ document.getElementById('registroEvento').addEventListener('submit', function(ev
       const hora = document.getElementById('hora').value;
 
       // Validaciones básicas
-      if (!nombre || !correo || !telefono || intereses.length === 0 || !horario) {
+      if (!nombre || !correo || !telefono || intereses.length === 0 || !horario || !fecha) {
         alert('Por favor, completa todos los campos obligatorios.');
         return;
       }
 
-      // Si todo está bien
+      // Validaciones nuevas 
+
+      // Validación A: el nombre debe tener al menos 3 caracteres (Sin contar espacios
+      // .trim() limpia los espacios invisibles al inicio y al final.
+      // .length cuenta cuántas letras quedaron.
+      if (nombre.trim().length < 3) {
+        alert('El nombre debe tener al menos 3 caracteres.');
+        return;
+      }
+
+     //Validación B: el correo debe parecer un correo real. 
+      if (!correo.includes('@') || !correo.includes('.')) {
+    alert('Error en Correo: Asegúrate de incluir un arroba (@) y un dominio válido (.com, .mx).');
+    return;
+     }
+
+     // Validación C: el teléfono debe ser un número de 10 dígitos.
+     // isNaN() significa "Is Not a Number" (¿No es un número?). Si el usuario escribe letras, lanzará error
+     if (telefono.length !== 10 || isNaN(telefono)) {
+    alert('Error en Teléfono: Debe contener exactamente 10 números, sin espacios ni letras.');
+    return;
+     }
+
+     // 4. Éxito
+     // Si el código logró llegar hasta aquí, significa que todas las validaciones pasaron.
       alert('Registro exitoso. ¡Gracias por registrarte!');
+
+     // Limpiar el formulario automáticamente después del éxito
+      document.getElementById('registroEvento').reset();
+
     });
