724e142378fa2f54e8a6c8b8cbc495dbedc22140
Emi-Ing04 <emigzmz@gmail.com>
2026-09-13 21:18:15 -0600

    feat: add image URLs for planets in the planetas module

--- Modulo3-DesarrolloAvanzadoEnJavaScript/CLASE 07-IntroductionNodejs-npm/Mi-exploracion-espacial/planetas.js
+++ Modulo3-DesarrolloAvanzadoEnJavaScript/CLASE 07-IntroductionNodejs-npm/Mi-exploracion-espacial/planetas.js
@@ -2,14 +2,21 @@ const planetas = [
   {
     nombre: "Titán",
     descripcion: "La luna más grande de Saturno, con lagos de metano.",
-    descubiertoEn: "1655"
+    descubiertoEn: "1655",
+    imagenUrl: "https://ichef.bbci.co.uk/ace/ws/800/cpsprodpb/3088/production/_107242421_002614447.jpg.webp"
   },
   {
     nombre: "Próxima Centauri b",
     descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
-    descubiertoEn: "2016"
+    descubiertoEn: "2016",
+    imagenUrl: "https://thumb.wikimedia.org/wikipedia/commons/thumb/b/b0/Proxima_Cen_b_versus_Earth.jpg/960px-Proxima_Cen_b_versus_Earth.jpg?utm_source=es.wikipedia.org&utm_campaign=imageinfo&utm_content=thumbnail"
+  },
+  {
+    nombre: "TRAPPIST-1e",
+    descripcion: "Un exoplaneta del tamaño de la Tierra que podría tener agua líquida.",
+    descubiertoEn: "2017",
+    imagenUrl: "https://upload.wikimedia.org/wikipedia/commons/3/38/TRAPPIST-1e_artist_impression_2018.png?utm_source=en.wikipedia.org&utm_campaign=imageinfo&utm_content=thumbnail_unscaled"
   }
-  // ¡Añade más planetas!
 ];
 
 module.exports = planetas;
