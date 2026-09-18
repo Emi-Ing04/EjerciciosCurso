5e80eac2009eca62005d03eb0ade59e93109371d
Emi-Ing04 <emigzmz@gmail.com>
2026-09-18 16:32:30 -0600

    feat: update Tarjeta component to display full name for better clarity; remove unused Tarjeta instance

--- Modulo4-IntroduccionReact/CLASE 01/mi-tarjeta/src/App.jsx
+++ Modulo4-IntroduccionReact/CLASE 01/mi-tarjeta/src/App.jsx
@@ -8,16 +8,9 @@ function App() {
       <div className="cards-wrapper">
         {/* Pass the data here. Using an initial or alias is a great way to protect your privacy online */}
         <Tarjeta 
-          nombre="E. Guzmán" 
+          nombre="Emilio Guzmán" 
           profesion="Ingeniero Mecánico" 
           mensaje="Especialista en mantenimiento y proyectos industriales." 
-        />
-        
-        {/* You can reuse the exact same component for someone else */}
-        <Tarjeta 
-          nombre="Ana Pérez" 
-          profesion="Desarrolladora Web" 
-          mensaje="¡Bienvenido a mi tarjeta de presentación!" 
         />
       </div>
     </div>
